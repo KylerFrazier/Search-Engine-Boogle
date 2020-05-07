8be8e8185c411d6daa70ae81f4ccf499890cd8a7
Andy Park <chanhyp1@uci.edu>
2020-05-06 17:09:22 -0700

    Finished indexing

---
+++ Indexer.py
@@ -0,0 +1,88 @@
+import os
+import json
+import pickle
+
+from nltk.tokenize import word_tokenize
+from nltk import FreqDist
+from bs4 import BeautifulSoup
+from collections import defaultdict
+
+from Posting import Posting
+
+class Indexer(object):
+
+    def __init__(self, DEV: str):
+
+        # If dir not found raise error
+        if not os.path.exists(DEV):
+            raise FileNotFoundError
+            
+        self.DEV = DEV
+        self.corpus = [sub.path for sub in os.scandir(self.DEV) if sub.is_dir()]
+
+        self.corpus = self.corpus[:1] # for testing
+        self.corpus = [os.path.join(sub, json_file) for sub in self.corpus \
+                            for json_file in os.listdir(sub) if os.path.isfile(os.path.join(sub, json_file))]
+
+    def get_batch(self, n_batch=3):
+    
+        n = int(len(self.corpus) / n_batch)
+        for idx in range(0, n_batch - 1):
+            yield self.corpus[idx * n : n * (idx + 1)]
+
+        yield self.corpus[n * (n_batch - 1):]
+
+    def index(self) -> None:
+        
+        docid = 0
+
+        for i, batch in enumerate(self.get_batch()):
+            HashTable = defaultdict(list)
+
+            for json_file in batch:
+                docid = docid + 1
+
+                with open(json_file, 'r') as document:
+                    data = json.load(document)
+
+                    self.save_document(docid, data['url'])
+
+                    tree = BeautifulSoup(data['content'], 'lxml')
+                    tokens = [token.lower() for token in word_tokenize(tree.get_text()) if len(token) >= 2]
+                    freq_dist = FreqDist(tokens)
+
+                    tokens = set(tokens) # Remove duplicates
+
+                    for token in tokens:
+                        HashTable[token].append(Posting(docid, freq_dist[token]))
+
+                del freq_dist
+                del data
+
+            with open(f"index-{i}.pickle", 'wb') as pickle_file:
+                pickle.dump(HashTable, pickle_file)
+
+            del HashTable
+            del batch
+
+    def save_document(self, docid: int, url: str) -> None:
+        
+        if not os.path.exists('document-id-convert.json'):
+            with open('document-id-convert.json', 'w') as json_file:
+                json.dump({docid: url}, json_file)
+
+        else:
+
+            with open('document-id-convert.json', 'r') as json_file:
+                convert = json.load(json_file)
+
+            if docid in convert.keys() or url in convert.values():
+                return
+
+            convert.update({docid: url})
+
+            with open('document-id-convert.json', 'w') as json_file:
+                json.dump(convert, json_file)
+
+p = Indexer('DEV')
+p.index()
